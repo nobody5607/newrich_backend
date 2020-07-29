34a1e51f09673e904bc989fb79a64f4144c72d9c
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2020-07-29 15:45:07 +0700

    shared

--- backend/modules/games/views/default/shared.php
+++ backend/modules/games/views/default/shared.php
@@ -1,18 +1,29 @@
 <?php
-
     use yii\helpers\Url;
     $link = 'https://api.newriched.com/';
     $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "%3A%2F$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $mainUrl = "{$link}/games/default/view-data?uuid=" . $id;
     $new_url = str_replace(":", "%3A", $mainUrl);
     $link_url = str_replace("/", "%2F", $new_url);
-
-    $this->title = 'รายละเอียด';
     Yii::$app->meta->keywords = isset($model->msg)?$model->msg:'';
     Yii::$app->meta->description = isset($model->msg)?$model->msg:'';
     Yii::$app->meta->image = $image;
-
 ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $model->msg; ?></title>
+    <meta property="og:url"           content="https://alfood.web.app/" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="<?= $model->msg; ?>" />
+    <meta property="og:description"   content="<?= $model->msg; ?>" />
+    <meta property="og:image"         content="<?= $image; ?>" />
+</head>
+<body>
+    
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -32,3 +43,5 @@
 
 
 </p>
+</body>
+</html>
