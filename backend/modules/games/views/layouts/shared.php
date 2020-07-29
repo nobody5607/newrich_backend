4c670313f7987a12fcc174c6eab1f2e7585521a9
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2020-07-29 16:04:30 +0700

    fix

--- backend/modules/games/views/layouts/shared.php
+++ backend/modules/games/views/layouts/shared.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->title; ?></title>
-    <title>วิธีทำปุ่ม Share Button ของ Facebook บนหน้าเพจ ให้แสดงรูปภาพให้ถูกต้อง (Update 2017)</title>
-
-     
     <?php $this->head() ?>
 </head>
 <body>
