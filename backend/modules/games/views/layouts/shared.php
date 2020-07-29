2e7c9e71ec62bfbc1b37c3fbac8409245f64d4f7
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2020-07-29 16:42:41 +0700

    fix

--- backend/modules/games/views/layouts/shared.php
+++ backend/modules/games/views/layouts/shared.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $this->title; ?></title>
-    <?php $this->head() ?>
+     
 </head>
 <body>
     <?= $content; ?>
